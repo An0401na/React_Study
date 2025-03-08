0c3e294c8ab9af7001831149e33e755ea48c5b33
안나 <dksek3050@gmail.com>
2025-03-09 02:13:31 +0900

    내가 생각한 Edting 버튼 눌렀을때의 상태제어

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,4 +1,3 @@
-
 import Player from "./components/Player.jsx";
 function App() {
   return (
--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 export default function Player ({name, symbol}){
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleIsEditing(editing){
+        setIsEditing(editing);
+    }
+
     return(
         <li>
-              <span className="player">
-                <span className="player-name">{name}</span>
+            <span className="player">
+                {isEditing ? <input/> : <span className="player-name">{name}</span>}
                 <span className="player-symbol">{symbol}</span>
-              </span>
-            <button>Edit</button>
+            </span>
+            {isEditing ? <button onClick={() => handleIsEditing(false)}>Save</button>
+                : <button onClick={() => handleIsEditing(true)}>Edit</button>}
         </li>
     );
 }

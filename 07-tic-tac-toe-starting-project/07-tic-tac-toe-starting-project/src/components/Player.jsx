45f41beedce1417b8aad611d005878457eaf1b6a
안나 <dksek3050@gmail.com>
2025-03-09 02:55:39 +0900

    이름 수정할때 양방향 바인딩 되도록 수정

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 
-export default function Player ({name, symbol}){
+export default function Player ({ initalName, symbol}){
     const [ isEditing, setIsEditing ] = useState(false);
+    const [ playerName, setPlayerName ] = useState(initalName);
 
     function handleIsEditing(){
         setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
-    if(isEditing){
-        playerName = <input type="text" required value={name}/>;
+    function handleChange(event){
+        setPlayerName(event.target.value);
+
     }
 
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if(isEditing){
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+    }
     return(
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
             </span>
             <button onClick={handleIsEditing}>{isEditing ? "Save" : "Edit"}</button>
         </li>

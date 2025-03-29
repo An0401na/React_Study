47221426c9b5ce1c817de944bdee43dfe4fc87b4
An Na <99172832+An0401na@users.noreply.github.com>
2025-03-29 18:35:48 +0900

    Input, Button  tailwind 적용

--- 06-Style/src/components/AuthInputs.jsx
+++ 06-Style/src/components/AuthInputs.jsx
@@ -1,24 +1,7 @@
 import { useState } from 'react';
-import {styled} from "styled-components";
 import Button from "./Button.jsx";
-import CustomInput from "./Input.jsx";
+import Input from "./Input.jsx";
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
-const TextButton = styled.button`
-    color: #f0b322;
-    border: none;
-
-  &:hover {
-    color: #f0920e;
-  }
-
-`
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -40,21 +23,21 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
-        <CustomInput label="Email" invalid={emailNotValid}
+    <div id="auth-inputs" className="w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800 mx-auto">
+      <div className="flex flex-col gap-2 mb-6 ">
+        <Input label="Email" invalid={emailNotValid}
             type="email"
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        <CustomInput label="Password" invalid={passwordNotValid}
+        <Input label="Password" invalid={passwordNotValid}
             type="password"
             onChange={(event) => handleInputChange('password', event.target.value)}
           />
-      </ControlContainer>
-      <div className="actions">
-        <TextButton type="button" >
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500" >
           Create a new account
-        </TextButton>
+        </button>
         <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>

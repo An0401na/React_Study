d6c392e42378b0863898df185e32cfb65d22162f
안나 <dksek3050@gmail.com>
2025-03-30 03:57:22 +0900

    프로젝트를 선택하지 않는 경우 NoProjectSelected로 , 선택했다면 ProjectDetail 컴포넌트 노출로 분기 처리

--- 09-Practice-Project-Project_Manage/src/App.jsx
+++ 09-Practice-Project-Project_Manage/src/App.jsx
@@ -1,16 +1,19 @@
 import SideBar from "./components/SideBar.jsx";
-import React from "react";
-import { Node } from "postcss";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from "react";
+import CreateProject from "./components/CreateProject.jsx";
+import ProjectDetail from "./components/ProjectDetail.jsx";
+
+const PROJECT = [{ name: "Learn React" }];
 
 function App() {
+  const [selectedProeject, setSelectedProeject] = useState();
+
   return (
-    <>
-      <main className="h-screen my-8 flex gap-8">
-        <SideBar />
-        <NoProjectSelected />
-      </main>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar projects={PROJECT} />
+      {!selectedProeject ? <NoProjectSelected /> : <ProjectDetail />}
+    </main>
   );
 }
 

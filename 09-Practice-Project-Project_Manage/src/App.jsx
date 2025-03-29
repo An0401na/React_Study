9aced40545e926d6fa82248c174f4bd4f5d15f2d
안나 <dksek3050@gmail.com>
2025-03-30 04:16:54 +0900

    사이드 바에서 프로젝트 선택시 selectedProejectf를 선택한 프로젝트를 저장하도록 수정

--- 09-Practice-Project-Project_Manage/src/App.jsx
+++ 09-Practice-Project-Project_Manage/src/App.jsx
@@ -4,14 +4,25 @@ import { useState } from "react";
 import CreateProject from "./components/CreateProject.jsx";
 import ProjectDetail from "./components/ProjectDetail.jsx";
 
-const PROJECT = [{ name: "Learn React" }];
+const PROJECT = [{ name: "Learn React" }, { name: "Learn React2" }];
 
 function App() {
   const [selectedProeject, setSelectedProeject] = useState();
 
+  if (selectedProeject) {
+    console.log("selectedProeject : " + selectedProeject.name);
+  }
+
+  function handleSelectedProjectClick(newSelectedProject) {
+    setSelectedProeject(newSelectedProject);
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar projects={PROJECT} />
+      <SideBar
+        projects={PROJECT}
+        onClickSelectedProject={handleSelectedProjectClick}
+      />
       {!selectedProeject ? <NoProjectSelected /> : <ProjectDetail />}
     </main>
   );
--- 09-Practice-Project-Project_Manage/src/components/SideBar.jsx
+++ 09-Practice-Project-Project_Manage/src/components/SideBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SideBar({ projects, props }) {
+function SideBar({ projects, onClickSelectedProject, props }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -13,8 +13,9 @@ function SideBar({ projects, props }) {
         {projects.map((project) => {
           return (
             <button
-              key={project}
+              key={project.name}
               className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800"
+              onClick={() => onClickSelectedProject(project)}
             >
               {project.name}
             </button>

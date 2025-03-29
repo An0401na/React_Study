5d9cd78e1fc88eb026cc903a92b46f7da1f867cb
안나 <dksek3050@gmail.com>
2025-03-30 02:50:36 +0900

    NoProjectSelected ui

--- 09-Practice-Project-Project_Manage/src/App.jsx
+++ 09-Practice-Project-Project_Manage/src/App.jsx
@@ -1,12 +1,14 @@
 import SideBar from "./components/SideBar.jsx";
 import React from "react";
+import { Node } from "postcss";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <SideBar></SideBar>
-        ...
+        <SideBar />
+        <NoProjectSelected />
       </main>
     </>
   );

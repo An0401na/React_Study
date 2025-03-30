105adc8fde47221c05912b267911cb58291e09bc
안나 <dksek3050@gmail.com>
2025-03-30 12:46:45 +0900

    projects 상태 추가 / 오타 수정

--- 09-Practice-Project-Project_Manage/src/App.jsx
+++ 09-Practice-Project-Project_Manage/src/App.jsx
@@ -4,41 +4,64 @@ import { useState } from "react";
 import CreateProject from "./components/CreateProject.jsx";
 import ProjectDetail from "./components/ProjectDetail.jsx";
 
-const PROJECT = [{ name: "Learn React" }, { name: "Learn React2" }];
+const PROJECTS = [
+  { title: "Learn React", description: "dfsdfse", dueDate: "" },
+  { title: "Learn React2", description: "dfsdfse", dueDate: "" },
+];
 
 function App() {
-  const [selectedProeject, setSelectedProeject] = useState();
+  const [projects, setProjects] = useState(PROJECTS);
+  const [selectedProject, setSelectedProject] = useState();
   const [isCreatingProject, setIsCreatingProject] = useState(false);
 
-  if (selectedProeject) {
-    console.log("selectedProeject : " + selectedProeject.name);
+  console.log(projects);
+  if (selectedProject) {
+    console.log("selectedProject : " + selectedProject.title);
   }
   console.log("isCreatingProject : " + isCreatingProject);
 
   function handleSelectedProjectClick(newSelectedProject) {
     setIsCreatingProject(false);
-    setSelectedProeject(newSelectedProject);
+    setSelectedProject(newSelectedProject);
   }
 
   function handleCreatingProjectClick() {
     setIsCreatingProject(true);
-    setSelectedProeject();
+    setSelectedProject();
+  }
+
+  function handleSaveProjectClick(
+    projectTitle,
+    projectDescription,
+    projectDueDate,
+  ) {
+    setProjects((prevProjects) => [
+      ...prevProjects,
+      {
+        title: projectTitle,
+        description: projectDescription,
+        dueDate: projectDueDate,
+      },
+    ]);
+    setIsCreatingProject(false); // 프로젝트 저장 후 다시 생성 모드 종료
   }
 
   let showComponent = (
     <NoProjectSelected onClickCreatingProject={handleCreatingProjectClick} />
   );
-  if (selectedProeject) {
-    showComponent = <ProjectDetail />;
+  if (selectedProject) {
+    showComponent = <ProjectDetail project={selectedProject} />;
   }
   if (isCreatingProject) {
-    showComponent = <CreateProject />;
+    showComponent = (
+      <CreateProject onClickSaveProject={handleSaveProjectClick} />
+    );
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
       <SideBar
-        projects={PROJECT}
+        projects={projects}
         onClickSelectedProject={handleSelectedProjectClick}
         onClickCreatingProject={handleCreatingProjectClick}
       />

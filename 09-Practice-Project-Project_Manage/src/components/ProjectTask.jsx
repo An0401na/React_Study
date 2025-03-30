18ce9175163799543988aa0fc0f066190e0c27d2
안나 <dksek3050@gmail.com>
2025-03-30 13:47:35 +0900

    task 여부 판단후 분기처리 & task 목록 출력

--- 09-Practice-Project-Project_Manage/src/components/ProjectTask.jsx
+++ 09-Practice-Project-Project_Manage/src/components/ProjectTask.jsx
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function ProjectTask(props) {
+function ProjectTask({ ref, project }) {
+  const task = useRef();
+
   return (
     <>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <div className="flex items-center gap-4">
-        <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
+        <input ref={task} className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
         <button className="text-stone-700 hover:text-stone-950">
           Add Task
         </button>
       </div>
-      <p className="text-stone-800 my-4">
-        This Project does not have any task yet.
-      </p>
-      <ul className="p-4 mt-8 rounded-md bg-stone-100">
-        <li className="flex justify-between my-4">
-          Learn the basics
-          <button className="text-stone-700 hover:text-red-500">Clear</button>
-        </li>
-        <li className="flex justify-between my-4">
-          Learn the basics
-          <button className="text-stone-700 hover:text-red-500">Clear</button>
-        </li>
-      </ul>
+      {!project.tasks ? (
+        <p className="text-stone-800 my-4">
+          This Project does not have any task yet.
+        </p>
+      ) : (
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {project.tasks.map((task, index) => {
+            return (
+              <li key={index} className="flex justify-between my-4">
+                {task}
+                <button className="text-stone-700 hover:text-red-500">
+                  Clear
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 }

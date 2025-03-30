cb96f00f783f7761fac50e942a15ab21f6ed22af
안나 <dksek3050@gmail.com>
2025-03-30 13:22:21 +0900

    Task 영역 ProjectTask 컴포넌트로 분리

--- 09-Practice-Project-Project_Manage/src/components/ProjectDetail.jsx
+++ 09-Practice-Project-Project_Manage/src/components/ProjectDetail.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ProjectTask from "./ProjectTask.jsx";
 
 function ProjectDetail({ project, onClickDeleteProject }) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
@@ -29,27 +30,7 @@ function ProjectDetail({ project, onClickDeleteProject }) {
             {project.description}
           </p>
         </header>
-        <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-        <div className="flex items-center gap-4">
-          <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-          <button className="text-stone-700 hover:text-stone-950">
-            Add Task
-          </button>
-        </div>
-
-        <p className="text-stone-800 my-4">
-          This Project does not have any task yet.
-        </p>
-        <ul className="p-4 mt-8 rounded-md bg-stone-100">
-          <li className="flex justify-between my-4">
-            Learn the basics
-            <button className="text-stone-700 hover:text-red-500">Clear</button>
-          </li>
-          <li className="flex justify-between my-4">
-            Learn the basics
-            <button className="text-stone-700 hover:text-red-500">Clear</button>
-          </li>
-        </ul>
+        <ProjectTask />
       </div>
     </>
   );

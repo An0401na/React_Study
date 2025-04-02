6f7e87af70f80fc636d400c61a47f05df1c9a5f1
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-02 18:20:42 +0900

    refactor: 프로젝트의 테스크 입력시 useRef 아닌 useState를 사용하여 값을 관리하도록 변경

--- 09-Practice-Project-Project_Manage/src/components/CreateProjectTask.jsx
+++ 09-Practice-Project-Project_Manage/src/components/CreateProjectTask.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateProjectTask({ taskInputRef, handleAddTaskClick }) {
+function CreateProjectTask({ handleAddTaskClick }) {
+  const [task, setTask] = useState();
+
+  function handleChange(event) {
+    setTask(event.target.value);
+  }
+  function handleClick() {
+    handleAddTaskClick(task);
+    setTask("");
+  }
   return (
     <div className="flex items-center gap-4">
       {/* 입력 필드 */}
       <input
-        ref={taskInputRef} // useRef로 입력 필드 값 추적
+        type="text"
+        onChange={handleChange}
+        value={task}
         className="w-64 px-2 py-1 rounded-sm bg-stone-200"
       />
       {/* 추가 버튼 */}
       <button
         className="text-stone-700 hover:text-stone-950"
-        onClick={handleAddTaskClick} // 클릭 시 작업 추가
+        onClick={handleClick} // 클릭 시 작업 추가
       >
         Add Task
       </button>
--- 09-Practice-Project-Project_Manage/src/components/ProjectTask.jsx
+++ 09-Practice-Project-Project_Manage/src/components/ProjectTask.jsx
@@ -4,17 +4,11 @@ import CreateProjectTask from "./CreateProjectTask.jsx";
 
 // ProjectTask 컴포넌트 - 프로젝트의 작업 리스트를 관리하는 컴포넌트
 function ProjectTask({ tasks, onClickAddTask, onClickClear }) {
-  // 작업 입력 필드를 위한 useRef로 상태 관리
-  const taskInputRef = useRef("");
-
   // 'Add Task' 버튼 클릭 시 실행되는 함수
-  function handleAddTaskClick() {
+  function handleAddTaskClick(task) {
     // 입력된 작업이 비어 있지 않으면 작업 추가
-    const taskValue = taskInputRef.current.value.trim(); // useRef에서 값 추출
-
-    if (taskValue !== "") {
-      onClickAddTask(taskValue); // 상위 컴포넌트에 작업 추가 요청
-      taskInputRef.current.value = ""; // 입력 필드 초기화
+    if (task.trim() !== "") {
+      onClickAddTask(task); // 상위 컴포넌트에 작업 추가 요청
     }
   }
 
@@ -25,7 +19,6 @@ function ProjectTask({ tasks, onClickAddTask, onClickClear }) {
 
       {/* 작업 입력 필드와 추가 버튼 */}
       <CreateProjectTask
-        taskInputRef={taskInputRef}
         handleAddTaskClick={handleAddTaskClick}
       ></CreateProjectTask>
 

a58859d55f4c6deb7f52331b84e859b9470df125
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-11 12:06:07 +0900

    #11 onConfirm 함수인 handleRemovePlace 함수를 useCallback 훅 사용하여 리렌더링시 재 생성되지 않도록 설정

--- 11-Side-Effects-useEffect/src/App.jsx
+++ 11-Side-Effects-useEffect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -65,7 +65,8 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
+    console.log("Removing place..." + selectedPlace.current);
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current),
     );
@@ -81,7 +82,7 @@ function App() {
         }),
       ),
     );
-  }
+  }, []);
 
   return (
     <>

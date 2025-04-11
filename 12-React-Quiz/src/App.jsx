763484312a7a98375d2dc2c8f56fc5da807a7aad
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-11 18:18:24 +0900

    #12 Todo 추가

--- 12-React-Quiz/src/App.jsx
+++ 12-React-Quiz/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header.jsx";
 import Quiz from "./components/Quiz.jsx";
 
 function App() {
+  //Todo: useState로 viewMode를 관리 (quiz, summary)
   return (
     <>
       <Header />

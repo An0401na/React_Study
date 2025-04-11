36946d52d9d6e3f543464dc84c96339da519bedc
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-11 10:52:12 +0900

    #11 ESC 입력으로 모달 닫힐때 함수 수정

--- 11-Side-Effects-useEffect/src/App.jsx
+++ 11-Side-Effects-useEffect/src/App.jsx
@@ -85,7 +85,7 @@ function App() {
 
   return (
     <>
-      <Modal open={modalIsOpen} onClose={handleRemovePlace}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}

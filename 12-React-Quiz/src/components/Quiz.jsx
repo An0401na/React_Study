5755f57053341aef0b76af373cc2f79983a2c757
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-11 18:11:50 +0900

    #12 Quiz 영역 ui

--- 12-React-Quiz/src/components/Quiz.jsx
+++ 12-React-Quiz/src/components/Quiz.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Quiz(props) {
-  return <div></div>;
+  return (
+    <section id="quiz">
+      <section id="question">
+        <progress value={3} max={10} />
+        <h2>How do you typically render list content in React apps?</h2>
+      </section>
+      <section id="answers">
+        <div className="answer">
+          <button>dfsf</button>
+        </div>
+        <div className="answer">
+          <button className="selected">dfsf</button>
+        </div>
+        <div className="answer">
+          <button className="correct">dfsf</button>
+        </div>
+        <div className="answer">
+          <button className="wrong">dfsf</button>
+        </div>
+      </section>
+    </section>
+  );
 }
 
 export default Quiz;

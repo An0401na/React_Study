b696b8a019c66572668ae728fe35806d4005a4b7
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-11 12:18:13 +0900

    #11 모달 창에 프로그래스 바 추가 & useEffect 활용하여 시간 측정시 무한루프 돌지 않도록 수정 & cleanup 함수 이용하여 컴포넌트 제거시 정리되도록 구현

--- 11-Side-Effects-useEffect/src/components/DeleteConfirmation.jsx
+++ 11-Side-Effects-useEffect/src/components/DeleteConfirmation.jsx
@@ -1,6 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 5000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTimer, setRemainingTimer] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Interval Timer tick ");
+      setRemainingTimer((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      console.log("Interval cleared");
+      clearInterval(interval);
+    }; // Cleanup 함수: 컴포넌트가 언마운트되거나 의존성이 변경될 때 실행되어 인터벌 타이머를 정리
+  }, []);
+
   useEffect(() => {
     console.log("Timer started");
     const timer = setTimeout(() => {
@@ -26,6 +42,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTimer} max={TIMER} />
     </div>
   );
 }

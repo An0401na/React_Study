6f2be5b52dfb36206ff6551097d6c6fc2fec9a3a
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-12 19:47:58 +0900

    #12 타이머 만료시 퀴즈 2개씩 넘어가는 문제 useEffect 사용하여 해결

--- 12-React-Quiz/src/components/Question.jsx
+++ 12-React-Quiz/src/components/Question.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const TIMER = 3000;
 function Question({ question, onTimeExpired }) {
-  console.log("Timer started");
-  const timer = setTimeout(() => {
-    onTimeExpired();
-  }, TIMER);
+  useEffect(() => {
+    console.log("Timer started");
+    const timer = setTimeout(() => {
+      onTimeExpired();
+    }, TIMER);
+
+    return () => {
+      console.log("Timer cleared");
+      clearTimeout(timer);
+    };
+  }, [onTimeExpired]);
 
   return (
     <section id="question">
-      <progress value={3} max={10} />
+      <progress value={3} max={TIMER} />
       <h2>{question}</h2>
     </section>
   );

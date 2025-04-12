6bfc37c4dc4ea464ccd5a0072765d87bd478df50
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-12 19:02:59 +0900

    #12 App 컴포넌트 렌더링 시 quiz 리스트 셔플하도록 수정

--- 12-React-Quiz/src/App.jsx
+++ 12-React-Quiz/src/App.jsx
@@ -1,14 +1,21 @@
 import Header from "./components/Header.jsx";
 import Quiz from "./components/Quiz.jsx";
 import Summary from "./components/Summary.jsx";
+import QUIZS from "./questions.js";
+import _ from "lodash";
 
 function App() {
+  console.log("App 재랜더링");
   //Todo: useState로 viewMode를 관리 (quiz, summary)
+
+  const shuffledQuizs = _.shuffle(QUIZS);
+  console.log(shuffledQuizs);
+
   return (
     <>
       <Header />
-      {/*<Quiz />*/}
-      <Summary />
+      <Quiz quizs={shuffledQuizs} />
+      {/*<Summary />*/}
     </>
   );
 }

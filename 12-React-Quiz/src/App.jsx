d88f4e8cbce4150af767c1d2f4e1ed5e5ed0e67e
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-12 21:39:15 +0900

    #12 퀴즈 제공 완료 후 summary 화면으로 이동하도록 viewMode 상태 추가

--- 12-React-Quiz/src/App.jsx
+++ 12-React-Quiz/src/App.jsx
@@ -3,19 +3,29 @@ import Quiz from "./components/Quiz.jsx";
 import Summary from "./components/Summary.jsx";
 import QUIZS from "./questions.js";
 import _ from "lodash";
+import { useState } from "react";
 
 function App() {
   console.log("App 재랜더링");
   //Todo: useState로 viewMode를 관리 (quiz, summary)
-
+  const [viewMode, setViewMode] = useState("quiz");
   const shuffledQuizs = _.shuffle(QUIZS);
   console.log(shuffledQuizs);
+
+  function handleViewModeChange(newViewMode) {
+    setViewMode(newViewMode);
+  }
 
   return (
     <>
       <Header />
-      <Quiz quizs={shuffledQuizs} />
-      {/*<Summary />*/}
+      {/*//Todo : shuffledQuizs로 바꾸기*/}
+      {/*  <Quiz quizs={shuffledQuizs} />*/}
+      {viewMode === "quiz" ? (
+        <Quiz quizs={QUIZS} onQuizEnd={handleViewModeChange} />
+      ) : (
+        <Summary />
+      )}
     </>
   );
 }
--- 12-React-Quiz/src/components/Quiz.jsx
+++ 12-React-Quiz/src/components/Quiz.jsx
@@ -2,14 +2,22 @@ import React, { useCallback, useEffect, useState } from "react";
 import Question from "./Question.jsx";
 import Answers from "./Answers.jsx";
 
-function Quiz({ quizs }) {
+function Quiz({ quizs, onQuizEnd }) {
   console.log("Quiz 리렌더링");
   const [currentIndex, setCurrentIndex] = useState(0);
   const quiz = quizs[currentIndex];
 
-  const handleNextQuestion = useCallback(function handleNextQuestion() {
-    setCurrentIndex((prevIndex) => prevIndex + 1);
-  }, []);
+  const handleNextQuestion = useCallback(
+    function handleNextQuestion() {
+      console.log("currentIndex", currentIndex);
+      if (currentIndex >= quizs.length - 1) {
+        onQuizEnd("summary");
+        return;
+      }
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    },
+    [currentIndex, quizs.length, onQuizEnd],
+  );
 
   return (
     <section id="quiz">

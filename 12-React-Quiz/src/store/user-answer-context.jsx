3f3874bf544cdfc40bc5314e74abcf8c1b5d9824
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-14 13:42:04 +0900

    #12 userAnswerContext 정의 및 userAnswerContextProvider 구현 (Reduce 사용 안함, State만 사용)

--- 12-React-Quiz/src/store/user-answer-context.jsx
+++ 12-React-Quiz/src/store/user-answer-context.jsx
@@ -1,34 +1,23 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useState } from "react";
 
 export const UserAnswerContext = createContext({
-  userAnswer: [],
+  userAnswer: [], // 문제, 선택한 답, 결과(skip, correct, wrong)를 값으로 하는 객체를 배열로 저장
   addUserAnswer: () => {},
 });
-// 문제 순서를 키로 하고 문제, 선택한 답, 결과를 값으로 하는 객체를 배열로 저장 skip, 정답 맞춤, 못맞춤 읠 결과로 둔다.
-// [{id: 0, answer: 0, result: "skip"}, {id: 1, answer: 1, result: "correct"}, {id: 2, answer: 2, result: "incorrect"}]
-
-function userAnswerContextReducer(state, action) {
-  return state;
-}
 
 export default function UserAnswerContextProvider({ children }) {
-  const [userAnswer, userAnswerDispatch] = useReducer(
-    userAnswerContextReducer,
-    {
-      userAnswer: [],
-    },
-  );
+  const [userAnswerState, setUserAnswerState] = useState([]);
 
-  function addUserAnswer(answer) {
-    const updatedUserAnswer = [...userAnswer.userAnswer];
-    updatedUserAnswer.push(answer);
-
-    return updatedUserAnswer;
+  function handleAddUserAnswer(answer) {
+    setUserAnswerState((prevUserAnswer) => {
+      const updatedUserAnswer = [...prevUserAnswer, answer];
+      return updatedUserAnswer;
+    });
   }
 
   const ctxValue = {
-    userAnswer: userAnswer,
-    addUserAnswer: addUserAnswer,
+    userAnswer: userAnswerState,
+    addUserAnswer: handleAddUserAnswer,
   };
 
   return (

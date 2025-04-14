5971a152308b14e45240c0838683e288dcd85e6a
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-14 18:03:56 +0900

    #12 조건부 렌더링 삼항연산자 제거

--- 12-React-Quiz/src/App.jsx
+++ 12-React-Quiz/src/App.jsx
@@ -29,11 +29,10 @@ function App() {
     <UserAnswerContextProvider>
       <Header /> {/* 상단 헤더 컴포넌트 표시 */}
       {/* viewMode 상태에 따라 퀴즈 화면 또는 결과 화면을 렌더링 */}
-      {viewMode === "quiz" ? (
-        <Quiz quizs={shuffledQuizs} onQuizEnd={handleViewModeChange} /> // 퀴즈 화면
-      ) : (
-        <Summary /> // 결과 화면
+      {viewMode === "quiz" && (
+        <Quiz quizs={shuffledQuizs} onQuizEnd={handleViewModeChange} />
       )}
+      {viewMode === "summary" && <Summary />}
     </UserAnswerContextProvider>
   );
 }

bea99bbce4087b3b7d00deeeebed5ef38c6cf820
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-14 14:50:29 +0900

    #12 주석 추가

--- 12-React-Quiz/src/store/user-answer-context.jsx
+++ 12-React-Quiz/src/store/user-answer-context.jsx
@@ -2,6 +2,7 @@ import { createContext, useReducer, useState } from "react";
 
 export const UserAnswerContext = createContext({
   userAnswer: [], // 문제, 선택한 답, 결과(skip, correct, wrong)를 값으로 하는 객체를 배열로 저장
+  // [{ question: "문제", selectedAnswer: "선택한 답", result: "skip" | "correct" | "wrong" }]
   addUserAnswer: () => {},
 });
 

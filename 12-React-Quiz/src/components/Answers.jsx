1289e6fc327ab3471d0139ecdf229bebf5a1d1e9
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-15 11:57:37 +0900

    #12 조건부 클래스 로직을 함수로 분리

--- 12-React-Quiz/src/components/Answers.jsx
+++ 12-React-Quiz/src/components/Answers.jsx
@@ -17,33 +17,34 @@ function Answers({
     onAnswerSelect(answer); // 부모 컴포넌트로 선택된 답안을 전달
   }
 
+  function getClassName(answer) {
+    // 사용자가 답을 선택한 단계라면
+    if (quizStage === "showSelectedAnswer") {
+      // 선택한 선택지 "selected" 클래스 추가
+      return answer === selectedAnswer ? "selected" : "";
+    }
+
+    // 정답을 보여주는 단계라면
+    if (quizStage === "showCorrectAnswer") {
+      // 정답인 보기에 "correct" 클래스 추가
+      if (answer === correctAnswer) return "correct";
+
+      // 사용자가 선택한 답이 정답이 아닐 경우 "wrong" 클래스 추가
+      if (answer === selectedAnswer && selectedAnswer !== correctAnswer)
+        return "wrong";
+    }
+
+    return "";
+  }
+
   return (
     <section id="answers">
       {/* 보기 목록을 렌더링 */}
       {answers.map((answer) => {
-        let className = "";
-
-        // 사용자가 답을 선택한 단계
-        if (quizStage === "showSelectedAnswer") {
-          // 선택한 보기만 강조
-          className = answer === selectedAnswer ? "selected" : "";
-        }
-        // 정답을 보여주는 단계
-        else if (quizStage === "showCorrectAnswer") {
-          // 사용자가 선택한 답이 정답이 아닐 경우 "wrong"
-          if (answer === selectedAnswer) {
-            className = "wrong";
-          }
-          // 정답인 보기에 "correct" 클래스 추가
-          if (answer === correctAnswer) {
-            className = "correct";
-          }
-        }
-
         return (
           <p key={answer} className="answer">
             <button
-              className={className} // 위 조건에 따라 동적으로 스타일 클래스 지정
+              className={getClassName(answer)} // 위 조건에 따라 동적으로 스타일 클래스 지정
               disabled={quizStage !== "quiz"} // 퀴즈 푸는 중에만 클릭 가능
               type="button"
               onClick={() => handleAnswerSelect(answer)} // 보기 선택 핸들러 호출

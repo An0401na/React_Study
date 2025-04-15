9d5934e020539088d01025c5f856403941f6dc1b
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-15 11:57:15 +0900

    #12 주석 수정

--- 12-React-Quiz/src/App.jsx
+++ 12-React-Quiz/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <>
       <Header /> {/* 상단 헤더 컴포넌트 표시 */}
-      // UserAnswerContextProvider로 감싸서 Quiz에서 사용자 답변 상태를 관리
+      {/*UserAnswerContextProvider로 감싸서 Quiz에서 사용자 답변 상태를 관리*/}
       <UserAnswerContextProvider>
         {/* viewMode 상태에 따라 퀴즈 화면 또는 결과 화면을 렌더링 */}
         {viewMode === "quiz" && (

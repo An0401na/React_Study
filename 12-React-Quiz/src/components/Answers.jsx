9bf7e17e0ee929eac26327b9b0dadf0945b417b0
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-15 11:48:53 +0900

    #12 주석 추가

--- 12-React-Quiz/src/components/Answers.jsx
+++ 12-React-Quiz/src/components/Answers.jsx
@@ -1,4 +1,10 @@
-
+// 답변 목록을 보여주는 컴포넌트
+// props:
+// - answers: 퀴즈의 선택지 목록
+// - selectedAnswer: 사용자가 고른 선택지
+// - correctAnswer: 퀴즈의 정답 선택지
+// - onAnswerSelect: 사용자가 답변 제출시 호출되는 함수
+// - quizStage: 퀴즈의 현재 상태 ("quiz" | "showSelectedAnswer" | "showCorrectAnswer")
 function Answers({
   answers,
   selectedAnswer,
@@ -6,21 +12,29 @@ function Answers({
   onAnswerSelect,
   quizStage,
 }) {
+  // 사용자가 답변을 선택했을 때 호출
   function handleAnswerSelect(answer) {
-    onAnswerSelect(answer);
+    onAnswerSelect(answer); // 부모 컴포넌트로 선택된 답안을 전달
   }
 
   return (
     <section id="answers">
+      {/* 보기 목록을 렌더링 */}
       {answers.map((answer) => {
         let className = "";
 
+        // 사용자가 답을 선택한 단계
         if (quizStage === "showSelectedAnswer") {
+          // 선택한 보기만 강조
           className = answer === selectedAnswer ? "selected" : "";
-        } else if (quizStage === "showCorrectAnswer") {
+        }
+        // 정답을 보여주는 단계
+        else if (quizStage === "showCorrectAnswer") {
+          // 사용자가 선택한 답이 정답이 아닐 경우 "wrong"
           if (answer === selectedAnswer) {
             className = "wrong";
           }
+          // 정답인 보기에 "correct" 클래스 추가
           if (answer === correctAnswer) {
             className = "correct";
           }
@@ -29,10 +43,10 @@ function Answers({
         return (
           <p key={answer} className="answer">
             <button
-              className={className}
-              disabled={quizStage !== "quiz"}
+              className={className} // 위 조건에 따라 동적으로 스타일 클래스 지정
+              disabled={quizStage !== "quiz"} // 퀴즈 푸는 중에만 클릭 가능
               type="button"
-              onClick={() => handleAnswerSelect(answer)}
+              onClick={() => handleAnswerSelect(answer)} // 보기 선택 핸들러 호출
             >
               {answer}
             </button>

65f01d6a0108ecdbc2b75e16d55c6edfae64607b
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-15 12:07:09 +0900

    #12 상수로 분리

--- 12-React-Quiz/src/components/Answers.jsx
+++ 12-React-Quiz/src/components/Answers.jsx
@@ -1,10 +1,12 @@
+import { STAGES } from "../constants/stages.js";
+
 // 답변 목록을 보여주는 컴포넌트
 // props:
 // - answers: 퀴즈의 선택지 목록
 // - selectedAnswer: 사용자가 고른 선택지
 // - correctAnswer: 퀴즈의 정답 선택지
 // - onAnswerSelect: 사용자가 답변 제출시 호출되는 함수
-// - quizStage: 퀴즈의 현재 상태 ("quiz" | "showSelectedAnswer" | "showCorrectAnswer")
+// - quizStage: 퀴즈의 현재 상태 ( STAGES.QUIZ |  STAGES.SELECTED |  STAGES.CORRECT)
 function Answers({
   answers,
   selectedAnswer,
@@ -19,13 +21,13 @@ function Answers({
 
   function getClassName(answer) {
     // 사용자가 답을 선택한 단계라면
-    if (quizStage === "showSelectedAnswer") {
+    if (quizStage === STAGES.SELECTED) {
       // 선택한 선택지 "selected" 클래스 추가
       return answer === selectedAnswer ? "selected" : "";
     }
 
     // 정답을 보여주는 단계라면
-    if (quizStage === "showCorrectAnswer") {
+    if (quizStage === STAGES.CORRECT) {
       // 정답인 보기에 "correct" 클래스 추가
       if (answer === correctAnswer) return "correct";
 
@@ -45,7 +47,7 @@ function Answers({
           <p key={answer} className="answer">
             <button
               className={getClassName(answer)} // 위 조건에 따라 동적으로 스타일 클래스 지정
-              disabled={quizStage !== "quiz"} // 퀴즈 푸는 중에만 클릭 가능
+              disabled={quizStage !== STAGES.QUIZ} // 퀴즈 푸는 중에만 클릭 가능
               type="button"
               onClick={() => handleAnswerSelect(answer)} // 보기 선택 핸들러 호출
             >

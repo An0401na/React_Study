ffc41c7fd224ff118edf22b90f8a4512c3d6cbc8
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-18 18:07:52 +0900

    #14 Class Component 기반으로 ErrorBoundary(오류경계) 컴포넌트 생성하여 예외처리하기

--- 14-Class-Component-Practice/src/components/UserFinder.js
+++ 14-Class-Component-Practice/src/components/UserFinder.js
@@ -4,6 +4,7 @@ import Users from "./Users";
 import classes from "./UserFinder.module.css";
 import { Component } from "react";
 import usersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 class UserFinder extends Component {
   static contextType = usersContext;
@@ -42,7 +43,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }

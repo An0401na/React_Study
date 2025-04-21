22e616c7231d716ded323dd53eefb63e698f70a7
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-21 18:20:48 +0900

    #15 사용자가 선택한 장소 삭제시 백엔드에서도 처리되도록 updateUserPlaces 함수 호출

--- 15-HTTP-Requests/src/App.jsx
+++ 15-HTTP-Requests/src/App.jsx
@@ -49,13 +49,29 @@ function App() {
     }
   }
 
-  const handleRemovePlace = useCallback(async function handleRemovePlace() {
-    setUserPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id),
-    );
+  const handleRemovePlace = useCallback(
+    async function handleRemovePlace() {
+      setUserPlaces((prevPickedPlaces) =>
+        prevPickedPlaces.filter(
+          (place) => place.id !== selectedPlace.current.id,
+        ),
+      );
 
-    setModalIsOpen(false);
-  }, []);
+      try {
+        await updateUserPlaces(
+          userPlaces.filter((place) => place.id !== selectedPlace.current.id),
+        );
+      } catch (error) {
+        setUserPlaces(userPlaces);
+        setErrorUpdatingPlaces({
+          message: error.message || "Failed to delete user places.",
+        });
+      }
+
+      setModalIsOpen(false);
+    },
+    [updateUserPlaces],
+  );
 
   function handleError() {
     setErrorUpdatingPlaces(null);

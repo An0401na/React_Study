1237da3cd83144ae5cf1f01d4db279a1eb0f74e0
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-21 17:49:22 +0900

    #15 사용자 장소 선택시 백엔드의 user-places.json에 저장 될 수 있도록 리액트 handleSelectPlace 수정

--- 15-HTTP-Requests/src/http.js
+++ 15-HTTP-Requests/src/http.js
@@ -13,3 +13,21 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }), // {places: places}와 동일
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user places.");
+  }
+
+  return resData.message;
+}

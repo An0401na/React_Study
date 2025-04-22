73cdedac6d077c4610a3962eda7f8295077aaf1d
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-22 09:53:07 +0900

    #15 백엔드에서 사용자가 선택한 장소 목록 가져올수 있도록 수정

--- 15-HTTP-Requests/src/http.js
+++ 15-HTTP-Requests/src/http.js
@@ -14,6 +14,22 @@ export async function fetchAvailablePlaces() {
   return resData.places;
 }
 
+export async function fetchUserPlaces() {
+  // Fetch 요청
+  const response = await fetch("http://localhost:3000/places");
+  console.log("📥 [fetch] 요청 성공");
+
+  // JSON 파싱
+  const resData = await response.json();
+  console.log("📦 [JSON 파싱 완료]", resData);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch places.");
+  }
+
+  return resData.places;
+}
+
 export async function updateUserPlaces(places) {
   const response = await fetch("http://localhost:3000/user-places", {
     method: "PUT",

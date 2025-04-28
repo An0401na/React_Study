6824df6c0ef2bef6c7f43c6146ed83af6b646b47
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-28 16:39:26 +0900

    #17 useRef 를 사용하여 사용자 입력데이터 제출시 검증하도록 수정

--- 17-Forms-User-Input/src/components/Login.jsx
+++ 17-Forms-User-Input/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const emailRef = useRef();
   const passwordRef = useRef();
 
@@ -12,8 +14,16 @@ export default function Login() {
     const password = passwordRef.current.value;
 
     console.log(email, password);
-  }
+    const emailIsValid = email.includes("@");
 
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+    console.log("Sending data to server...");
+  }
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -22,6 +32,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={emailRef} />
+          <div className="control-error">
+            {emailIsInvalid && <p>유효한 이메일 주소를 입력해 주세요.</p>}{" "}
+          </div>
         </div>
 
         <div className="control no-margin">

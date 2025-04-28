604c066fc8b8d17e410a32c15154f6c503b2b3b7
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-28 18:21:39 +0900

    #17 input 영역을 컴포넌트화하여 재사용 할 수 있게 수정 & 패스워드 검증 추가

--- 17-Forms-User-Input/src/components/StateLogin.jsx
+++ 17-Forms-User-Input/src/components/StateLogin.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Input from "./Input.jsx";
 
 export default function Login() {
   const [enteredValue, setEnteredValue] = useState({
@@ -12,6 +13,8 @@ export default function Login() {
   });
 
   const emailIsInvalid = didEdit.email && !enteredValue.email.includes("@");
+  const passwordIsInvalid =
+    didEdit.password && enteredValue.password.trim().length < 6;
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -42,33 +45,28 @@ export default function Login() {
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onBlur={() => handleInputBlur("email")} // 포커스를 잃을 때마다 발현
-            onChange={(event) => handleInputChange("email", event.target.value)}
-            value={enteredValue.email}
-          />
-          <div className="control-error">
-            {emailIsInvalid && <p>유효한 이메일 주소를 입력해 주세요.</p>}{" "}
-          </div>
-        </div>
-
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-            value={enteredValue.password}
-          />
-        </div>
+        <Input
+          label="Email"
+          id="email"
+          type="text"
+          name="email"
+          onBlur={() => handleInputBlur("email")} // 포커스를 잃을 때마다 발현
+          onChange={(event) => handleInputChange("email", event.target.value)}
+          value={enteredValue.email}
+          error={emailIsInvalid && "유효한 이메일 주소를 입력해 주세요."}
+        />
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onBlur={() => handleInputBlur("password")} // 포커스를 잃을 때마다 발현
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+          value={enteredValue.password}
+          error={passwordIsInvalid && "비밀번호는 6자 이상이어야 합니다."}
+        />
       </div>
 
       <p className="form-actions">

10cfe3fc068d1d548af68a2de57f684bad5c5297
An Na <99172832+An0401na@users.noreply.github.com>
2025-04-29 10:42:34 +0900

    #17 사용자 정보 관련 로직을 useInput으로 커스텀 훅으로 관리

--- 17-Forms-User-Input/src/components/StateLogin.jsx
+++ 17-Forms-User-Input/src/components/StateLogin.jsx
@@ -1,47 +1,30 @@
-import { useState } from "react";
 import Input from "./Input.jsx";
 import { hasMinLength, isEmail, isNotEmpty } from "../util/validation.js";
+import { useInput } from "../hooks/useInput.js";
 
 export default function Login() {
-  const [enteredValue, setEnteredValue] = useState({
-    email: "",
-    password: "",
-  });
-  const [didEdit, setDidEdit] = useState({
-    // 사용자와 상호작용을 통해 포커스를 잃었는지 저장
-    email: false,
-    password: false,
-  });
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
 
-  const emailIsInvalid =
-    didEdit.email &&
-    !isEmail(enteredValue.email) &&
-    !isNotEmpty(enteredValue.email);
-  const passwordIsInvalid =
-    didEdit.password && !hasMinLength(enteredValue.password, 6);
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError,
+  } = useInput("", (value) => hasMinLength(value, 6));
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log("submit");
-    console.log(enteredValue);
-  }
+    if (emailHasError || passwordHasError) {
+      return;
+    }
 
-  function handleInputChange(identifier, value) {
-    setEnteredValue((prevValues) => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
-    setDidEdit((prevValues) => ({
-      ...prevValues,
-      [identifier]: false,
-    }));
-  }
-
-  function handleInputBlur(identifier) {
-    setDidEdit((prevValues) => ({
-      ...prevValues,
-      [identifier]: true,
-    }));
+    console.log(emailValue, passwordValue);
   }
 
   return (
@@ -54,22 +37,20 @@ export default function Login() {
           id="email"
           type="text"
           name="email"
-          onBlur={() => handleInputBlur("email")} // 포커스를 잃을 때마다 발현
-          onChange={(event) => handleInputChange("email", event.target.value)}
-          value={enteredValue.email}
-          error={emailIsInvalid && "유효한 이메일 주소를 입력해 주세요."}
+          onBlur={handleEmailBlur} // 포커스를 잃을 때마다 발현
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && "유효한 이메일 주소를 입력해 주세요."}
         />
         <Input
           label="Password"
           id="password"
           type="password"
           name="password"
-          onBlur={() => handleInputBlur("password")} // 포커스를 잃을 때마다 발현
-          onChange={(event) =>
-            handleInputChange("password", event.target.value)
-          }
-          value={enteredValue.password}
-          error={passwordIsInvalid && "비밀번호는 6자 이상이어야 합니다."}
+          onBlur={handlePasswordBlur} // 포커스를 잃을 때마다 발현
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && "비밀번호는 6자 이상이어야 합니다."}
         />
       </div>
 

634f61f9175a8b1d370f79d78373df5b3a10f3dc
An Na <99172832+An0401na@users.noreply.github.com>
2025-05-07 16:06:49 +0900

    #20 비교 연산자 수정

--- 20-Redux-Basics-2/src/store/index.js
+++ 20-Redux-Basics-2/src/store/index.js
@@ -2,11 +2,11 @@ import { createStore } from "redux";
 
 // 리듀스 함수
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type == "INCREMENT") {
+  if (action.type === "INCREMENT") {
     return { counter: state.counter + 1 };
   }
 
-  if (action.type == "DECREMENT") {
+  if (action.type === "DECREMENT") {
     return { counter: state.counter - 1 };
   }
 

8ee8b382eae8be29dcc6829d9d85cd7f796ab06f
An Na <99172832+An0401na@users.noreply.github.com>
2025-05-07 13:58:50 +0900

    #20 구독 함수 지정

--- 20-Redux-Basics-1/redux-demo.js
+++ 20-Redux-Basics-1/redux-demo.js
@@ -1,6 +1,5 @@
 const redux = require("redux");
 
-const counterReducer = (state, action) => {
   return { count: state.count + 1 };
 };
 
@@ -8,5 +7,8 @@ const store = redux.createStore(counterReducer); // redux 저장소 생성
 
 const counterSubscriber = () => {
   // 현재 상태를 가져옴 (구독중)
-  store.getState(); // 상태가 변경될때 마다 트리거 되어 최신 상태로 변경
+  const latestState = store.getState(); // 상태가 변경될때 마다 트리거 되어 최신 상태로 변경
+  console.log("구독중 : ", latestState);
 };
+
+store.subscribe(counterSubscriber);

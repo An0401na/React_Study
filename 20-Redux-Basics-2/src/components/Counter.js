d3ffc56f75d9aa4d8e7da3bc43cef7e734feb96d
An Na <99172832+An0401na@users.noreply.github.com>
2025-05-07 16:26:51 +0900

    #20 increment, decrement 버튼 생성하여 액션 연결

--- 20-Redux-Basics-2/src/components/Counter.js
+++ 20-Redux-Basics-2/src/components/Counter.js
@@ -1,14 +1,28 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>INCREMENT</button>
+        <button onClick={decrementHandler}>DECREMENT</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );

0bd8595982e57c6eaba84d2744cdc71a70157318
An Na <99172832+An0401na@users.noreply.github.com>
2025-05-07 13:27:12 +0900

    #20 리듀서 생성

--- 20-Redux-Basics-1/redux-demo.js
+++ 20-Redux-Basics-1/redux-demo.js
@@ -1,3 +1,7 @@
-const redux = require('redux');const redux = require("redux");
+const redux = require("redux");
+
+const counterReducer = (state, action) => {
+  return { count: state.count + 1 };
+};
 
 const store = redux.createStore(); // redux 저장소 생성

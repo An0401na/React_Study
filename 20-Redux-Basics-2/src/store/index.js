94ecd755bb2cbd1d2e6d84e7fd7c9416346a4d33
An Na <99172832+An0401na@users.noreply.github.com>
2025-05-09 14:31:45 +0900

    #20 기존 리듀스 함수를 slice로 변경

--- 20-Redux-Basics-2/src/store/index.js
+++ 20-Redux-Basics-2/src/store/index.js
@@ -1,29 +1,25 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
-// 리듀스 함수
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT") {
-    return { counter: state.counter + 1, showCounter: state.showCounter };
-  }
 
-  if (action.type === "INCREASE") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "DECREMENT") {
-    return { counter: state.counter - 1, showCounter: state.showCounter };
-  }
-
-  if (action.type === "TOGGLE") {
-    return { counter: state.counter, showCounter: !state.showCounter };
-  }
-
-  return state;
-};
+const countSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    increase(state, action) {
+      state.counter += action.amount;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 // 스토어 생성
 const store = createStore(counterReducer);

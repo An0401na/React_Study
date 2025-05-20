8fac22e2633104c166879b4d704440b3bf86a524
An Na <99172832+An0401na@users.noreply.github.com>
2025-05-20 15:51:39 +0900

    #21 redux 의 notification 상태를 조건부 랜더링

--- 21-Advanced-Redux/src/App.js
+++ 21-Advanced-Redux/src/App.js
@@ -1,25 +1,27 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useEffect } from "react";
-import { uiAction } from "./store/ui-slice";
+import { Fragment, useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
 
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    dispatch(
-      uiAction.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data!",
-      }),
-    );
     const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        }),
+      );
       const response = await fetch("http://localhost:4000/cart", {
         method: "PUT",
         body: JSON.stringify(cart),
@@ -30,7 +32,7 @@ function App() {
       }
 
       dispatch(
-        uiAction.showNotification({
+        uiActions.showNotification({
           status: "success",
           title: "Success!",
           message: "Sending cart data successfully!",
@@ -38,9 +40,9 @@ function App() {
       );
     };
 
-    sendCartData().catch((error) => {
+    sendCartData().catch(() => {
       dispatch(
-        uiAction.showNotification({
+        uiActions.showNotification({
           status: "error",
           title: "Error!",
           message: "Sending cart data failed.",
@@ -50,10 +52,19 @@ function App() {
   }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
